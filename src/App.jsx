3d01598338bf179f8428cba331e5e31ddb1ae646
Carla Valdivia <valdivia.carla@gmail.com>
2023-12-18 15:24:13 -0300

    react new + die component + style

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+import './App.css'
+import Die from './Die'
+
+function App() {
+
+  // generates random number (6 - 1)
+
+  function numbersGenerator() {
+    const numbersArray = []
+    for(let i = 0; i < 10; i+=1) {
+      const newNumber = Math.ceil(Math.random() * 6);
+      numbersArray.push(newNumber);
+    }
+    return numbersArray;
+  }
+
+  // generates new numbers for new game on click
+
+  function rollDice() {
+    setDice(numbersGenerator())
+  }
+
+  // Initializes dice state with random numbers
+  const[dice, setDice] = useState(numbersGenerator())
+
+  // Creates instances of Dice components
+
+ const diceElements = dice.map((number, index) => <Die key={index + 1} value={number} />)
+
+  return (
+    <main>
+      <div className="container">
+        <h1 className="main-title"> Tenzies</h1>
+        <p className="main-subtitle">
+          Roll until all dice are the same. Click each dice to freeze it at its
+          current value between rolls.
+        </p>
+
+        <div className="dice-container">
+          {diceElements}
+        </div>
+
+        <div className="btn-container">
+          <button className="btn-play" onClick={rollDice}>Roll</button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App
